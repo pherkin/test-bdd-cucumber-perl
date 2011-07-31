72f1b8aaf8980ef78c2967cd29b233123c0d0134
Peter Sergeant <pete@clueball.com>
2011-07-31 21:16:51 +0100

    Better docs coming in

--- lib/Test/BDD/Cucumber.pm
+++ lib/Test/BDD/Cucumber.pm
@@ -1,27 +1,52 @@
-package Test::Pherkin;
+package Test::BDD::Cucumber;
 
 =head1 NAME
 
-Test::Pherkin - Feature-complete Cucumber / Gherkin / BDD style testing in Perl
+Test::BDD::Cucumber - Feature-complete Cucumber-style testing in Perl
 
 =head1 DESCRIPTION
 
-A sane and complete BDD implementation in Perl, using Gherkin, and inspired by
-Cucumber.
+A sane and complete Cucumber implementation in Perl
 
-=head1 GLOSSARY
+=head1 NEXT STEPS
 
-B<BDD> - Behaviour-Driven Development. A keyword used by Agile Testing
-Consultants and other less specialized villains and evil-doers.
+If you are B<completely new to Cucumber>, you'd get a pretty overview from
+reading our short and crunchy L<Tutorial|Test::BDD::Cucumber::Manual::Tutorial>.
 
-B<Gherkin> -
+If you B<already understand Cucumber>, and just want to get started> then you
+should read the L<Step-writing quick-start
+guide|Test::BDD::Cucumber::Manual::Steps>, the documentation for our
+command-line tool L<App::Pherkin>, and L<How to integrate with
+Test::Builder|Test::BDD::Cucumber::Manual::Integration>.
 
-=head1 THE CODE IS SOLID, THE CONCEPT IS INSANE
+If you B<want to extend or integrated Test::BDD::Cucumber> then you'd probably
+be more interested in our L<Architecture
+overview|Test::BDD::Cucumber::Manual::Architecture>.
 
-Basing your test suite or your testing strategy on Cucumber-style tests is
-batshit nuts. If you're using them as anything more than a shiny toy to dazzle
-gullible project managers, you need to have your head checked. This viewpoint
-has not prejudiced this library, however, which was written to be relied on.
+=head1 BUGS AND MISSING
+
+The following things do not work in this release, although support is planned
+in the very near future:
+
+=over 4
+
+=item * Tags
+
+=item * Localization
+
+=item * Transforms
+
+=item * Quoting in tables is broken
+
+=back
+
+=head1 AUTHOR
+
+Peter Sergeant - L<pete@clueball.com>
+
+=head1 LICENSE ETC
+
+Copyright 2011 Peter Sergeant. Released under the same terms as Perl itself.
 
 =cut
 
